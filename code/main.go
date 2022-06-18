5360cfd1acbaa724a7d9594671ec7e79da77fcb1
rino_pk <rino@pk.com>
2022-06-18 16:06:58 +0900

    blockchain 途中

---
+++ code/main.go
@@ -0,0 +1,27 @@
+package main
+
+import ("fmt")
+
+
+type Block struct{
+	Timestamp int64
+	Data []byte
+	PrevBlockHazsh []byte
+	Hash []byte
+}
+
+func (b *Block) SetHash(){
+	fmt.Println(strconv.FormatInt(b.Timestamp, 10))
+
+	timestamp :=[]byte(strconv.FormatInt(b.Timestamp, 10))
+
+	headers := bytes.Join([][]byte{b.PreBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+
+func main(){
+	fmt.Println("test")
+}
